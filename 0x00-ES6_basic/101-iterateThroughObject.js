0bc0eaf1278c54204380bdd1075bbb75410272ff
alexagirma <alexagirma255@gmail.com>
2022-06-17 13:45:54 +0000

    updated

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,3 +1,9 @@
 export default function iterateThroughObject(reportWithIterator) {
-  return
- }
+  const employees = [];
+
+  for (const employee of reportWithIterator) {
+    employees.push(employee);
+  }
+
+  return employees.join(' | ');
+}
