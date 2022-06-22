8dc2c7a9687478d369ca836aab89df61d554ebbf
alexagirma <alexagirma255@gmail.com>
2022-06-22 12:23:43 +0000

    updated

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
-export default function getStudentIdsSum () {
-    return array.reduce((accumulator, i) => accumulator + i.id, 0);
+export default function getListStudentIds(array) {
+  return array.reduce((accumulator, i) => accumulator + i.id, 0);
 }
